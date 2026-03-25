a9dcd432a0e57d463a739307a207d35a754a918f
CE0S23 <ras3ec@gmail.com>
2026-03-24 20:17:41 -0600

    fix: register AppController in AppModule

--- ceosmos-api/src/app.module.ts
+++ ceosmos-api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
@@ -24,5 +26,7 @@ import { SessionsModule } from './sessions/sessions.module';
     UsersModule,
     MediaModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
